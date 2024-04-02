7c75f6b8b1784398263c6297aeccde5a463430dd
akv5974 <konstantin.alekhnovich@yandex.ru>
2024-04-02 10:52:37 +0500

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,10 @@
+def print_params(word):
+    print(word)
+    print(word)
+
+
+print_params('слово')
+print_params(100500)
+print_params(['q', 'w', 'e', 'r', 't', 'y'])
+print_params({1: 'q', 2: 'w', 3: 'e', 4: 'r'})
+print_params(('x', 'y', 'z'))
